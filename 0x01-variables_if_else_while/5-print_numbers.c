0cf22a916fa2c96994de885dfe8fd291999ce9af
mina ezzat <minaezzat98@gmail.com>
2023-08-11 16:06:34 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print numbers from 0 to 9
+* Return: alwys 0 in c language
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
