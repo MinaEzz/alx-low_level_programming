ec0625af5c45f4b6b88721c535e798371158f7ac
mina ezzat <minaezzat98@gmail.com>
2023-08-11 15:33:03 -0700

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - print lowecase alphapet
+* main - print lowecase alphapet
 * Return: alwys 0 in c language
 */
 int main(void)
 {
-          int a;
-	  int A;
- 
-          for (a = 97; a <= 122; a++)
-          {
-                  putchar(a);
-          }
-	  for (A = 65; A <= 90; A++)
-          {
-                  putchar(A);
-          }
-	  putchar('\n');
-          return (0);
+	int a;
+	int A;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (A = 65; A <= 90; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
 }
