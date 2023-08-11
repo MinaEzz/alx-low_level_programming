21fad3149d89ca851b3d08ceaa79b73cbee5d06e
mina ezzat <minaezzat98@gmail.com>
2023-08-11 15:48:42 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print lowercase alphapet except q and e
+* Return: always 0 in C
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n == 101 || n == 113)
+			continue;
+		putchar(n);
+	}
+	return (0);
+}
