25ca7d1a20b985b371ee1d274b2bb6092720f11a
mina ezzat <minaezzat98@gmail.com>
2023-08-11 17:05:29 -0700

    c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print combinations of single-digit numbers
+* Return: alwys 0 in c language
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n == 57)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
