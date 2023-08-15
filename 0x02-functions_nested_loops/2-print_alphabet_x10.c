6b779d911b54f31d75503ef451b892940092d60d
mina ezzat <minaezzat98@gmail.com>
2023-08-15 16:33:17 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print lowercase 10 times
+* Return: alwys 0 in c language
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int h;
+
+	for (i = 1; i <=10 ; i++)
+	{
+		for (h = 97; h <= 122; h++)
+		{
+			_putchar(h);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,5 @@
 #include "putchar.c"
-#include "1-alphabet.c"
+#include "2-print_alphabet_x10.c"
 /**
  * main - check the code
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_alphabet();
+    print_alphabet_x10();
     return (0);
 }
