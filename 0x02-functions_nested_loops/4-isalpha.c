31aa2ed45c61a4662c695ef71828fb6bf49371ce
mina ezzat <minaezzat98@gmail.com>
2023-08-15 17:02:44 -0700

    c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _isalpha - checks for alphabetic character
-* @c function parameter
+* @c: function parameter is a character
 * Return: 1 if c is a letter , 0 otherwise
 */
 int _isalpha(int c)
