33e09c9c1d0c697135e56babda973918d2782a2c
mina ezzat <minaezzat98@gmail.com>
2023-08-17 06:05:20 -0700

    c debugging

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - print sizeof
+* Return: alwys 0 in c language
+*/
+void jack_bauer(void)
+{
+	int a, b, x, y;
+
+	for(a = 0; a < 3; a++)
+	{
+		for(b = 0; b < 4; b++)
+		{
+
+		}
+	}
+}
