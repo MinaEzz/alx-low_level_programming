72243dba94a5f6101b108b2898cd77e4d90a358a
mina ezzat <minaezzat98@gmail.com>
2023-08-18 01:31:22 -0700

    c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * main - Fizz-Buzz test
+  * Return: always 0 in c programming
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	return (0);
+}
