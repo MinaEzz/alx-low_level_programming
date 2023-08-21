7e9508249d3faeeb1cc9ca4784ebfa82f7faa806
mina ezzat <minaezzat98@gmail.com>
2023-08-21 07:03:01 -0700

    c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,14 @@
 */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i]; i++)
+	while (s[i])
+		i++
 
-		for ( ; ; i--)
-		{
-			_putchar(s[i]);
-		}
+			for ( ; ; i--)
+			{
+				_putchar(s[i]);
+			}
 	_putchar('\n');
 }
