b15206ad6baf5fed6b04221c196b75e82ebaf6c9
mina ezzat <minaezzat98@gmail.com>
2023-08-21 06:42:57 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_rev -  prints a string, in reverse
+* @s: function's paramete
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		while (i--)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
