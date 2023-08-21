561d5fadaa294666fa4e149562715f2389575767
mina ezzat <minaezzat98@gmail.com>
2023-08-21 04:40:12 -0700

    c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - swaps the values of two integers
+* @a: function's first param
+* @b: function's second param
+*/
+void swap_int(int *a, int *b)
+{
+	int *p;
+
+	*p = *a;
+	*a = *b;
+	*b = *p;
+}
