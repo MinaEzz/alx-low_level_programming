a1a0772dfbdb126028b643db8b153a11a1658544
mina ezzat <minaezzat98@gmail.com>
2023-08-21 05:31:38 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen - function that returns the length of a string
+* @s: function's paramete
+* Return: length of string
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+	for (i = 0; *s[i] != 0; i++)
+	{
+		length++;
+	}
+	return (length);
+}
