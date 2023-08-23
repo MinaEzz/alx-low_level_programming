327e43bab70a89aea70715df69edc44510b543c9
mina ezzat <minaezzat98@gmail.com>
2023-08-23 16:31:05 -0700

    c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* *_strcat - concatenate src to dest
+* @dest: function's paramete which is a string
+* @src: function's parameter which is a string
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; dest[i] > 0; i++)
+	{
+		while(src[j] != '\0')
+		{
+			dest[i + j] = src[j];
+		}
+	}
+
+	return (dest);
+}
