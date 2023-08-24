f8eebb4176bcb9e21a2a074248d0831e960eb80e
mina ezzat <minaezzat98@gmail.com>
2023-08-24 02:14:25 -0700

    c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 /**
 * *_strcat - concatenate src to dest
 * @dest: function's paramete which is a string
@@ -10,7 +10,7 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j = 0;
 
-	for (i = 0; dest[i] > 0; i++)
+	for (i = 0; dest[i] >= 0; i++)
 	{
 		while (src[j] != '\0')
 		{
