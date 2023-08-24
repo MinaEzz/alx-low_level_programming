fac8f724a860047b77617d79d6728dbdde313389
mina ezzat <minaezzat98@gmail.com>
2023-08-24 02:30:17 -0700

    c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j = 0;
 
-	for (i = 0; dest[i] != 0; i++)
+	for (i = 0; dest[i] > 0; i++)
 	{
-		while (src[j] != '\0')
+		while (src[j] > 0)
 		{
 			dest[i + j] = src[j];
 			j++;
