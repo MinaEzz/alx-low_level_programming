48f4627f4892f8d71ec2616651f5d73a47d4095e
mina ezzat <minaezzat98@gmail.com>
2023-08-24 03:46:42 -0700

    c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		(n > i) ? dest[i] = src[i] : break;
 	}
-	dest[i] = '\0';
+	while (n > i)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
