051dd641225c02f43b364e3a10f38acf9ec96c9e
mina ezzat <minaezzat98@gmail.com>
2023-08-25 08:05:15 -0700

    c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* reverse_array - revers array
+* @a: function's parameter array
+* @n: number of array element
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+	int last_element = n - 1;
+	int new_array[n];
+
+	for (i = last_element; i >= 0; i--)
+	{
+		new_array = a[i];
+	}
+}
