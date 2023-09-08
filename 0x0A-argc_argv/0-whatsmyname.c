89cae42c4fe4308babc0854f8e22d068df845e80
mina ezzat <minaezzat98@gmail.com>
2023-09-08 04:44:02 -0700

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - prints name followed by a new line
+*@argc: function's param
+*@argv: function's param
+*Return: always 0 in c 
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
