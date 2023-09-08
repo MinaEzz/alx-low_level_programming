e5538e3655e7866ad3c243d181772bcea2b2b8fb
mina ezzat <minaezzat98@gmail.com>
2023-09-08 04:52:41 -0700

    c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - prints all arguments it receives
+*@argc: function's parameter
+*@argv: function's parameter
+*Return: always 0 in c
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[x]);
+		i++;
+	}
+
+	return (0);
+}
